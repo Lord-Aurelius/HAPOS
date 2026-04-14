54ccc14762f3cb4dcf92e9d450883a8701681834
Lord Aurelius <kelvin.houseaurelius@gmail.com>
2026-04-14 07:27:27 +0300

    your update

--- android/app/src/main/java/Com/houseaurelius/hapos/MainActivity.java
+++ android/app/src/main/java/Com/houseaurelius/hapos/MainActivity.java
@@ -1,5 +1,26 @@
 package com.houseaurelius.hapos;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+        if (webView == null) {
+            return;
+        }
+
+        WebSettings settings = webView.getSettings();
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setDisplayZoomControls(false);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+    }
+}
